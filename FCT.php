b155beda67c6643899e187f6c49202745970495b
AAAAC-bb <charlanders1@gmail.com>
2016-05-12 23:50:07 +0800

    database update

---
+++ FCT.php
@@ -0,0 +1,7 @@
+<?php
+	$mysql_server_name="localhost"; 
+    $mysql_username="";
+    $mysql_password="";
+    $conn=mysql_connect($mysql_server_name, $mysql_username,$mysql_password);
+    mysql_select_db("");
+?>
